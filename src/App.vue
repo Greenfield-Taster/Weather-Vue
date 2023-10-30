9732501e4b44eb3abe4415e7eaf882a1015b0542
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-10-30 19:07:03 +0200

    Add current user location, out weather on this location

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="myWeather">
+    <button @click="getLocation()">Get my weather</button>
+
+    <table>
+      <thead>
+        <td>
+          {{ city }}
+        </td>
+        <td>
+          {{ country }}
+        </td>
+        <td>[{{ latitude }}; {{ longitude }}]</td>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ico</td>
+          <td>humidity</td>
+          <td>{{ Weather.humidity }}</td>
+        </tr>
+        <tr>
+          <td>ico</td>
+          <td>temp</td>
+          <td>{{ Weather.temp }}</td>
+        </tr>
+        <tr>
+          <td>ico</td>
+          <td>main</td>
+          <td>{{ Weather.main }}</td>
+        </tr>
+        <tr>
+          <td>ico</td>
+          <td>pressure</td>
+          <td>{{ Weather.pressure }}</td>
+        </tr>
+        <tr>
+          <td>ico</td>
+          <td>description</td>
+          <td>{{ Weather.description }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted, ref } from "vue";
+import axios from "axios";
+
+const city = ref("");
+const country = ref("");
+const latitude = ref("");
+const longitude = ref("");
+
+const Weather = ref({
+  humidity: "",
+  temp: "",
+  main: "",
+  pressure: "",
+  description: "",
+});
+
+const getLocation = () => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+};
+const showPosition = (position) => {
+  latitude.value = position.coords.latitude;
+  longitude.value = position.coords.longitude;
+  // const apiKey = "pk.dfc77c3e733a06fba2855876c491f105";
+  axios
+    .get(
+      `https://us1.locationiq.com/v1/reverse?lat=${latitude.value}&lon=${longitude.value}&format=json&key=pk.dfc77c3e733a06fba2855876c491f105`
+    )
+    .then((response) => {
+      city.value = response.data.address.city;
+      country.value = response.data.address.country;
+      console.log(city.value);
+      console.log(country.value);
+
+      getWeatherInfo(position);
+    })
+    .catch((error) => console.log("Міста не знайдено", error));
+};
+const getWeatherInfo = (position) => {
+  latitude.value = position.coords.latitude;
+  longitude.value = position.coords.longitude;
+
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude.value}&lon=${longitude.value}&appid=7914d5a440960cfd5df3bd0388a7ad0f`
+    )
+    .then((response) => {
+      Weather.value.humidity = response.data.main.humidity;
+      Weather.value.temp = response.data.main.temp;
+      Weather.value.main = response.data.weather[0].description;
+      Weather.value.pressure = response.data.main.pressure;
+      Weather.value.description = response.data.weather[0].description;
+    });
+};
+onMounted(() => {
+  getLocation();
+  getWeatherInfo();
+});
+</script>
+<style scoped></style>
